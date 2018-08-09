ab2dd59b335c8389249a2eb4662c149b760c4ccb
Simon Gustafsson <simon.gustafsson@jayway.com>
2018-08-09 17:00:27 +0200

    Init Commit
    Initial commit of OauthPOC

---
+++ ADPocAct/ADPocAct/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using System.Security.Claims;
+
+namespace ADPocAct.Controllers
+{
+    [Authorize]
+    [RoutePrefix("")]
+    public class HelloController : ApiController
+    {
+        [Route("")]
+        public string GetHelloWorld()
+        {
+            string owner = ClaimsPrincipal.Current.FindFirst(ClaimTypes.Name).Value;
+            return $"Hello {owner}";
+        }
+    }
+}
---
+++ ADPocAct/ADPocAct/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using System.Web.Cors;
+using System.Web.Http;
+using ADPocAct;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+[assembly:Microsoft.Owin.OwinStartup(typeof(Startup))]
+namespace ADPocAct
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidAudience = ""
+                    },
+                    Tenant = ""
+                });
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+            //app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions());
+           /* app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions()
+            {
+                ClientId = "",
+                Authority = ""
+            });*/
+ 
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
